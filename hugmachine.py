06dc8b19c0e00d1c88a3544b8f2e4a45031475ed
Awsome-Areej <41355424+Awsome-Areej@users.noreply.github.com>
2018-07-17 21:51:57 -0700

    Create hugmachine.py

---
+++ hugmachine.py
@@ -0,0 +1,7 @@
+def hugmachine():
+    name = input ("What Is Your Name?Type In Quotation Marks!")
+    yesno = input ("Do You Wanna Hug?Please type yes or no , please print in lowercase letters and quotation marks!")
+    if yesno == "yes":
+        print ("If You Want A Hug "  +   name + " So, "  + " Go To Who Ever Is Around , Ask Do You Want A Hug! " )
+    else:
+        print("I Hope Next Time " +  name +  " You Will Want A Hug!")
